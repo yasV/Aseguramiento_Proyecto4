a4f1dca38d1228cbb9920127fbc3458e049fb888
Yasiell Vallejos <yasi1205@hotmail.com>
2016-11-13 19:23:19 -0600

    Base Proyecto

---
+++ Proyecto4_ServicioCorreo/EmailService_Classes/notificaciones.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailService_Classes
+{
+    interface notificaciones
+    {
+    }
+}
