d2ac0fce9c42d4c07b829bb720714646d75c6f67
Amanda Sofia Solano Astorga <Amanda Sofia Solano Astorga>
2016-11-19 20:35:36 -0600

    Resend and send email test

--- Proyecto4_ServicioCorreo/EmailService_Classes/servicesInterface.cs
+++ Proyecto4_ServicioCorreo/EmailService_Classes/servicesInterface.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace EmailService_Classes
 {
-    interface servicesInterface
+    public interface servicesInterface
     {
-        bool sendEmail(List<emailAddress> emailList, string mensaje); //enviar Email
+        bool sendEmail(emailAddress emailList, string mensaje); //enviar Email
         void reSendEmail(List<emailAddress> emailList, string mensaje, int numIntentos); //reenviar email
         void createNotificationDaily(string hora, string mensaje); //crear notificacion diaria, y llama la funcion writefle
         void createNotificationMonthly(int dia ,string hora, string mensaje); 
         void createNotificationWeekly(int dia, string hora, string mensaje);
         void executeNotification(); // lee archivo, crea lista, ordena y revista
         void writeFile(string nombre, string notoficacion); // escribe en el archivo
+        int getintent();
     }
 }
---
+++ Proyecto4_ServicioCorreo/EmailService_Test/sendEmail_Test.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmailService_Classes;
+using Mail4Net;
+using Moq;
+
+
+namespace EmailService_Test
+{
+    [TestFixture]
+    public class sendEmail_Test
+    {
+        [TestCase("yasi1205@hotmail.com")]
+        [TestCase("assa2909@gmail.com")]
+        [TestCase("nels@gmail.com")]
+           
+    [Test]
+        public void sendCorrectEmail(String email)
+        {
+            var client = new Mail4Net.Client.FakeClient();
+            var from = "assa2909@gmail.com";
+            var to = email;
+            var subject = "Esto es una prueba";
+            var body = "Esto es solamente un caso de prueba";
+
+            client.Send(from, to, subject, body);
+
+            bool succesfull = client.MailSent;
+
+            Assert.IsTrue(succesfull);
+        }
+
+       
+        [TestCase("assa2909@gmail.com","Amanda")]
+        [Test]
+    public void resendEmail(String email,String name)
+        {
+            List<emailAddress> resendTest = new List<emailAddress>();
+            resendTest.Add(new emailAddress(name, email));
+            DateTime hora = DateTime.Now;
+            var mockCall = new Mock<servicesInterface>();
+            mockCall.Setup(mock => mock.reSendEmail(resendTest, "Esto es una prueba", 1));
+            hora.AddHours(2);
+            mockCall.Verify(mock => mock.reSendEmail(resendTest, "Esto es una prueba", 2));
+            hora.AddHours(2);
+            mockCall.Verify(mock => mock.reSendEmail(resendTest, "Esto es una prueba", 3));
+            var result = mockCall.Setup(x => x.getintent());
+            Assert.AreEqual(result, 3);
+
+        }
+    
+    
+ 
+    }
+}
