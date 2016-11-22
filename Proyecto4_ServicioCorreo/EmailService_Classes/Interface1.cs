5e8b6e6f42d0595a76d2a831822f4d9d6ea80455
Amanda Sofia Solano Astorga <Amanda Sofia Solano Astorga>
2016-11-22 11:05:42 -0600

    Api call for http method

---
+++ Proyecto4_ServicioCorreo/EmailService_Classes/Interface1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailService_Classes
+{
+    public interface IHttpWebRequestFactory
+    {
+        HttpWebRequest Create(string uri);
+    }
+}
